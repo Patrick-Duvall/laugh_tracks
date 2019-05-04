ac4010ad102a5a6c91abf1a1453ee38aef832cb7
Patrick Duvall <duvallp208@gmail.com>
2019-05-04 15:45:29 -0600

    Refactor Age-query to include age filter

--- spec/features/user_sees_age_queried_comedians_spec.rb
+++ spec/features/user_sees_age_queried_comedians_spec.rb
@@ -2,6 +2,7 @@ require "rails_helper"
 
 describe "user sees age-queried comedians" do
   describe "they visit /comedians?age=22" do
+
     it "displays all comedians aged 22 " do
 
       comedian_1 = Comedian.create(name: "george", age: 22, city: 'Montreal')
@@ -9,7 +10,10 @@ describe "user sees age-queried comedians" do
       Special.create(name: "Walk Your Way Out", runtime_mins: 70, comedian_id: comedian_1.id)
       Special.create(name: "Walk Your Way Out", runtime_mins: 80, comedian_id: comedian_1.id)
 
-      visit '/comedians?age=22'
+      visit '/comedians'
+      save_and_open_page
+      fill_in 'age', with: '22'
+      click_button 'Filter'
       expect(page).to have_content(comedian_1.name)
       expect(page).to have_content(comedian_1.age)
       expect(page).to have_content(comedian_1.city)
@@ -17,7 +21,7 @@ describe "user sees age-queried comedians" do
       expect(page).to have_no_content(comedian_2.age)
       expect(page).to have_no_content(comedian_2.city)
 
-      save_and_open_page
+
       expect(page).to have_content("Specials Count: 2")
       expect(page).to have_content("Average Runtime: 75.0 mins")
 
